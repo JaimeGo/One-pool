27224fbccd353cc1f198e636f9cec7d10709d385
Jaime González <jgonzalez1@uc.cl>
2018-05-19 17:38:46 -0400

    Test image

--- canvas.js
+++ canvas.js
@@ -1,8 +1,8 @@
 "use strict";
 
 function Canvas2D() {
-    this._canvas = null;
-    this._canvasContext = null;
+    this._canvas = document.getElementById("screen");
+    this._canvasContext = this._canvas.getContext('2d');
     this._canvasOffset = Vector2.zero;
 }
 
@@ -11,10 +11,15 @@ Canvas2D.prototype.clear = function () {
 };
 
 
-Canvas2D.prototype.drawImage = function (sprite, position, rotation, scale, origin) {
+Canvas2D.prototype.drawImage = function (image, position, rotation, scale, origin) {
 
-    this._canvasContext.drawImage(sprite, 0, 0,
+    this._canvasContext.drawImage(image, 0, 0,
         sprite.width, sprite.height,
         -origin.x * scale, -origin.y * scale,
         sprite.width * scale, sprite.height * scale);
 };
+
+let Canvas=new Canvas2D()
+
+let image=new Image()
+image.src="./assets/sprites/spr_backgound.png"
