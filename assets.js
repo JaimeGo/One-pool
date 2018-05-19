49d1d69351f3b30ace2513f95376a85fdf3d4eff
Jaime González <jgonzalez1@uc.cl>
2018-05-19 18:29:17 -0400

    Update

--- assets.js
+++ assets.js
@@ -1,27 +1,27 @@
-let sprites={}
-let assetsStillLoading=0
+let sprites={};
+let assetsStillLoading=0;
 
 function assetsLoadingLoop(callback){
 	if(assetsStillLoading){
-		requestAnimationFrame(assetsLoadingLoop.bind(this,callback))
+		requestAnimationFrame(assetsLoadingLoop.bind(this,callback));
 	}
 	else{
-		callback()
+		callback();
 	}
 }
 
 
 function loadAssets(callback){
 	function loadSprite(filename){
-		assetsStillLoading++
-		let spriteImage=new Image()
-		spriteImage.src="./assets/sprites/"+filename
+		assetsStillLoading++;
+		let spriteImage=new Image();
+		spriteImage.src='./assets/sprites/'+filename;
 		spriteImage.onload=function(){
-			assetsStillLoading--
-		}
+			assetsStillLoading--;
+		};
 	}
-	sprites.background=loadSprite('spr_background.png')
-	sprites.stick=loadSprite("spr_stick.png")
+	sprites.background=loadSprite('spr_background5.png');
+	sprites.stick=loadSprite('spr_stick.png');
 
-	assetsLoadingLoop(callback)
+	assetsLoadingLoop(callback);
 }
---
+++ stick.js
@@ -0,0 +1,14 @@
+function Stick(){
+	this.position={x:0,y:400}
+}
+
+Stick.prototype.update=function(){
+
+
+}
+
+Stick.prototype.draw=function(){
+
+	Canvas.drawImage(sprites.background, {x:0,y:0})
+}
+
